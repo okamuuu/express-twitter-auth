a9e64afe442ba2ca4f6d7e5168d2a3ab42e7f8f3
okamuuu <okamuuu@gmail.com>
2019-02-28 16:13:52 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,85 @@
+var createError = require('http-errors');
+var express = require('express');
+var session = require('express-session');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+
+var config = require('./config')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'secret-key',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport-twitterの設定
+passport.use(new TwitterStrategy({
+    consumerKey: config.CLIENT_ID,
+    consumerSecret: config.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+  },
+  // 認証後の処理
+  function(token, tokenSecret, profile, done) {
+    return done(null, profile);
+  }
+));
+// セッションに保存
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+
+app.use('/', indexRouter);
+app.use('/user', usersRouter);
+app.get('/auth/twitter', passport.authenticate('twitter'));
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/?auth_failed' }), 
+  function (req, res) {
+    res.redirect('/user');
+  });
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+if (!module.parent) {
+  app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+  });
+}
+
+module.exports = app;
